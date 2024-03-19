7236b6357f1cc8bbcb9a57fd768e191f8c61c7fb
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-03-19 11:37:57 +0000

    Modified place.py

--- models/place.py
+++ models/place.py
@@ -34,26 +34,37 @@ class Place(BaseModel, Base):
                                      ForeignKey('amenities.id'),
                                      primary_key=True))
 
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey('cities.id'))
-    user_id = Column(String(60), ForeignKey('users.id'))
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
+        __tablename__ = "places"
+        city_id = Column(String(60), ForeignKey('cities.id'))
+        user_id = Column(String(60), ForeignKey('users.id'))
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review", backref="place",
                                cascade="all, delete")
         amenities = relationship("Amenity", backref="place_amenities",
                                  secondary="place_amenity",
                                  viewonly=False)
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             """ Returns the list of Review instances with
