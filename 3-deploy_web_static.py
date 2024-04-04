709d266a84f1d4116bd6aec7ebc9b03004c0ef52
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-04-04 15:44:21 +0000

    Added some files

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Creates and distributes an archive to your web servers """
+from fabric.api import put, run, env, local
+from os import os.path
+
+
+def do_pack():
+    """ Generates a .tgz archive """
+
+    local("mkdir -p versions/")
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = "versions/web_static_{}.tgz".format(time)
+    res = local("tar -cvzf {} web_static".format(path))
+    if res.failed:
+        return None
+    return path
+
+
+def do_deploy(archive_path):
+    """ Distributes an archive to your web servers """
+
+    env.user = 'ubuntu'
+    env.hosts = ['52.86.133.238', '52.87.211.253']
+
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, '/tmp/ ')
+        archive_path = archive_path.split('/')[1].strip('.tgz')
+        path = "/data/web_static/releases/{}".format(archive_path)
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{}.tgz -C {}/'.format(archive_path, path))
+        run('rm /tmp/{}.tgz'.format(archive_path))
+        run('mv {}/web_static/* {}/'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}/ /data/web_static/current'.format(path))
+        return True
+    except as err:
+        return False
+
+
+def deploy():
+    """ Creates and distributes an archive to your web servers """
+
+    path = do_pack()
+    if path is None:
+        return False
+    return do_deploy(path)
