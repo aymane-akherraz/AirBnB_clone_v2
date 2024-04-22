e9e1e496252e6ef82f9d66763d952c18df4a7c73
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-04-22 23:55:09 +0100

    Modified 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Starts a Flask web app """
+""" Flask web app """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -14,12 +13,11 @@ def dispose(exception):
     storage.close()
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states():
     """ Display list of all the states """
     states = storage.all(State)
-    states_list = list(states.values())
-    return render_template('7-states_list.html', states=states_list)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
