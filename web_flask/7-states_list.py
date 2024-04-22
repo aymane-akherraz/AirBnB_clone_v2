47c90436da0918eced695c7af156d8c46f94140e
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-04-22 23:52:02 +0100

    Modified 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-""" Flask web application """
+""" Starts a Flask web app """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ Removes the current SQL Session """
-
+def dispose(exception):
+    """ Remove current session """
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def list_states():
-    """ List availabale states when requesting /states_list """
-
-    res = storage.all(State)
-    return render_template('7-states_list.html', states=res)
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
